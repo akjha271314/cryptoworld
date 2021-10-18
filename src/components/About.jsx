d9437259d26e43623f050243eb503e65db165371
akjha271314 <akjha271314@gmail.com>
2021-10-18 14:31:31 +0530

    Everything completed except about us

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Card, Typography } from 'antd';
+import { LinkedinOutlined, GithubOutlined, MailOutlined } from '@ant-design/icons';
+
+const { Title } = Typography;
 
 const About = () => {
     return (
         <div>
-            About Us
+            <Card>
+                <Title level={3}>About Us</Title>
+            </Card>
         </div>
     )
 }
--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
 
 const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100;
@@ -16,16 +17,19 @@ const Cryptocurrencies = ({ simplified }) => {
         const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
         setCryptos(filteredData);
-        
+
     },[cryptosList, searchTerm]);
     
-    if(isFetching) return 'Loading...'
+    if(isFetching) return <Loader />;
 
     return (
         <>
-            <div className="search-crypto">
+            {!simplified && (
+                <div className="search-crypto">
                 <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
-            </div>
+                </div>
+            )}
+        
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency)=> (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
